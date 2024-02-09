1c5819e5f4543b4b5c836d601da10714dddb21cd
ValentynaSira1 <113180300+ValentynaSira1@users.noreply.github.com>
2024-02-09 20:11:14 +0000

    Deploying to gh-pages from @ ValentynaSira1/video@db422ee55c0ac2b36ea81d1432fe7c1377294131 🚀

--- index.a58e0615.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".image-slider",{simulateTouch:!0,touchRatio:1,touchAngle:45,grabCursor:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},hashNavigation:{watchState:!0},mousewheel:{sensitivity:1,eventsTarget:".image-slider"},slidesPerView:4,slidesPerGroup:2,autoHeight:!0,spaceBetween:0});
-//# sourceMappingURL=index.a58e0615.js.map
