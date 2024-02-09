db422ee55c0ac2b36ea81d1432fe7c1377294131
Валентина Сіра <ivachenko2010@ukr.net>
2024-02-09 22:10:50 +0200

    *

--- src/video.js
+++ src/video.js
@@ -1,29 +1,29 @@
-// import Player from '@vimeo/player'; 
-// import throttle from 'lodash.throttle'; 
+import Player from '@vimeo/player'; 
+import throttle from 'lodash.throttle'; 
 
-// const { default: Swiper } = require("swiper");
+const { default: Swiper } = require("swiper");
 
  
-// const iframe = document.querySelector('iframe'); 
-// const player = new Player(iframe); 
+const iframe = document.querySelector('iframe'); 
+const player = new Player(iframe); 
  
-// const onPlay = function (data) { 
-//     localStorage.setItem('videoplayer-current-time', data.seconds); 
-// }; 
+const onPlay = function (data) { 
+    localStorage.setItem('videoplayer-current-time', data.seconds); 
+}; 
  
-// player.on('timeupdate', throttle(onPlay, 1000)); 
+player.on('timeupdate', throttle(onPlay, 1000)); 
  
-// const currentTime = Number(localStorage.getItem('videoplayer-current-time')); 
+const currentTime = Number(localStorage.getItem('videoplayer-current-time')); 
  
-// player.setCurrentTime(currentTime).then(function (seconds) {  
-// }).catch(function (error) { 
-//     switch (error.name) { 
-//         case 'RangeError':  
-//             break; 
-//         default: 
-//             break; 
-//     } 
-// }); 
+player.setCurrentTime(currentTime).then(function (seconds) {  
+}).catch(function (error) { 
+    switch (error.name) { 
+        case 'RangeError':  
+            break; 
+        default: 
+            break; 
+    } 
+}); 
 
 
 new Swiper('.image-slider', {
